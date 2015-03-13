cc0a3fb3d8943280b2f752354f7f6c3b746e7763
emilymacleod <margaretemacleod@gmail.com>
2015-03-13 12:00:27 -0300

    recording capability
    recording capability

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,136 +1,135 @@
 angular.module('starter.controllers', ['ngStorage', 'ngCordova'])
 
-.controller('LoginCtrl', function($scope, $http, $cordovaOauth, $localStorage, $location) {
+    .controller('LoginCtrl', function($scope, $http, $cordovaOauth, $localStorage, $location) {
 
-    $scope.data = {};
+        $scope.data = {};
 
-    $scope.login = function() {
+        $scope.login = function() {
 
-        // log in the user through the api.  Please see the example "uitSamplePassportApi" for more information
-        $http.post("http://OAUTH_API_IP_OR_DOMAIN/login", { email: $scope.data.email, password: $scope.data.password }).then(function(result) {
-            if (result.data.loginstatus == "success") {
+            // log in the user through the api.  Please see the example "uitSamplePassportApi" for more information
+            $http.post("http://OAUTH_API_IP_OR_DOMAIN/login", { email: $scope.data.email, password: $scope.data.password }).then(function(result) {
+                if (result.data.loginstatus == "success") {
 
-                // successful login, in our example, we will just send an alert message
-                alert("Congrats, you logged in with user ID "+result.data.userid);
+                    // successful login, in our example, we will just send an alert message
+                    alert("Congrats, you logged in with user ID "+result.data.userid);
 
-            }
-            else {
+                }
+                else {
 
-                // unsuccessful login.  In our example, we are just sending an alert message
-                alert(result.data.message);
-            }
-        }, function(error) {
-            alert("There was a problem getting your profile.  Check the logs for details.");
-            console.log(error);
-        });
-    }
-
-    // function to use oAuth to get a token from Facebook.  The token will then be saved in local storage
-    // so that it can be re-used
-    $scope.facebookLogin = function() {
-
-        // Check local storage first by doing a check on a current access token and expiry
-        // If the token isn't expired, redirect to grab Facebook Profile
-        if ($localStorage.hasOwnProperty("accessToken") && parseInt($localStorage.expiry) > Math.round(new Date().getTime())) {
-            $location.path("/facebookProfile");
-        }
-        else {
-
-            // use cordovaOauth to connect to facebook and retrieve an access token
-            // CLIENT_ID_HERE: This variable requires the App ID of your facebook app which you will use to gather information.
-            // Please see http://developers.facebook.com/ for more information on setting up an example app
-            $cordovaOauth.facebook("CLIENT_ID_HERE", ["email", "read_stream", "user_website", "user_location", "user_relationships"]).then(function(result) {
-                
-                // Save the access token and expiry in local storage
-                $localStorage.accessToken = result.access_token;
-                $localStorage.expiry = Math.round(new Date().getTime()) + parseInt(result.expires_in);
-
-                // Redirect to the facebook profile page (see FacebookProfileCtrl)
-                $location.path("/facebookProfile");
-
-            }, function(error) {
-
-                // Just displaying an alert on login issue and logging in console.log
-                alert("There was a problem signing in!  See the console for logs");
-                console.log(error);
-            });
-        }
-    };
-})
-
-.controller('FacebookProfileCtrl', function($scope, $http, $localStorage, $location) {
-
-    // init function called on load of Facebook Profile page
-    $scope.init = function() {
-
-        // if the access token is available, grab the user's profile information
-        if($localStorage.hasOwnProperty("accessToken") === true) {
-
-            //retrieve profile information
-            $http.get("https://graph.facebook.com/v2.2/me", { params: { access_token: $localStorage.accessToken, fields: "id,name,gender,location,website,picture,relationship_status", format: "json" }}).then(function(result) {
-                $scope.profileData = result.data;
+                    // unsuccessful login.  In our example, we are just sending an alert message
+                    alert(result.data.message);
+                }
             }, function(error) {
                 alert("There was a problem getting your profile.  Check the logs for details.");
                 console.log(error);
             });
-        } else {
-            alert("Not signed in");
-            $location.path("/login");
         }
-    };
-})
 
+        // function to use oAuth to get a token from Facebook.  The token will then be saved in local storage
+        // so that it can be re-used
+        $scope.facebookLogin = function() {
 
+            // Check local storage first by doing a check on a current access token and expiry
+            // If the token isn't expired, redirect to grab Facebook Profile
+            if ($localStorage.hasOwnProperty("accessToken") && parseInt($localStorage.expiry) > Math.round(new Date().getTime())) {
+                $location.path("/facebookProfile");
+            }
+            else {
 
+                // use cordovaOauth to connect to facebook and retrieve an access token
+                // CLIENT_ID_HERE: This variable requires the App ID of your facebook app which you will use to gather information.
+                // Please see http://developers.facebook.com/ for more information on setting up an example app
+                $cordovaOauth.facebook("CLIENT_ID_HERE", ["email", "read_stream", "user_website", "user_location", "user_relationships"]).then(function(result) {
 
+                    // Save the access token and expiry in local storage
+                    $localStorage.accessToken = result.access_token;
+                    $localStorage.expiry = Math.round(new Date().getTime()) + parseInt(result.expires_in);
 
+                    // Redirect to the facebook profile page (see FacebookProfileCtrl)
+                    $location.path("/facebookProfile");
 
-/*
+                }, function(error) {
 
-    .controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
-        $scope.errorMessage = "";
-        $scope.data = {};
-        $scope.login = function() {
-
-
-            LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
-                console.log(data.status);
-                if (data.status == "error"){
-                    console.log("error Message");
-                    $scope.errorMessage = "Could not login";
-                    $state.go("signIn");
-                }
-                else {
-                    console.log("success");
-                    $state.go('tab.profile');
-                }
-            }).error(function(data) {
-                var alertPopup = $ionicPopup.alert({
-                    title: 'Login failed!',
-                    template: 'Please check your credentials!'
+                    // Just displaying an alert on login issue and logging in console.log
+                    alert("There was a problem signing in!  See the console for logs");
+                    console.log(error);
                 });
-            });
-        }
-    })*/
+            }
+        };
+    })
+
+    .controller('FacebookProfileCtrl', function($scope, $http, $localStorage, $location) {
+
+        // init function called on load of Facebook Profile page
+        $scope.init = function() {
+
+            // if the access token is available, grab the user's profile information
+            if($localStorage.hasOwnProperty("accessToken") === true) {
+
+                //retrieve profile information
+                $http.get("https://graph.facebook.com/v2.2/me", { params: { access_token: $localStorage.accessToken, fields: "id,name,gender,location,website,picture,relationship_status", format: "json" }}).then(function(result) {
+                    $scope.profileData = result.data;
+                }, function(error) {
+                    alert("There was a problem getting your profile.  Check the logs for details.");
+                    console.log(error);
+                });
+            } else {
+                alert("Not signed in");
+                $location.path("/login");
+            }
+        };
+    })
+
+
+
+
+
+
+    /*
+     .controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
+     $scope.errorMessage = "";
+     $scope.data = {};
+     $scope.login = function() {
+     LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+     console.log(data.status);
+     if (data.status == "error"){
+     console.log("error Message");
+     $scope.errorMessage = "Could not login";
+     $state.go("signIn");
+     }
+     else {
+     console.log("success");
+     $state.go('tab.profile');
+     }
+     }).error(function(data) {
+     var alertPopup = $ionicPopup.alert({
+     title: 'Login failed!',
+     template: 'Please check your credentials!'
+     });
+     });
+     }
+     })*/
 
     .controller('CameraCtrl', function($scope, $state) {
-  $scope.imageURI = 'http://www.dvinfo.net/forum/attachments/view-video-display-hardware-software/4853d1193613730-smpte-color-bars-bars_pal.jpg';
-  $scope.takePhoto = function() {
-var cameraOptions = {
-      targetWidth: 300,
-      targetHeight: 300
-    };
-    navigator.camera.getPicture(function(imageURI) {
-      
-      $scope.imageURI = imageURI;
-      $state.go('tab.camera');
+        $scope.imageURI = 'http://www.dvinfo.net/forum/attachments/view-video-display-hardware-software/4853d1193613730-smpte-color-bars-bars_pal.jpg';
+        $scope.takePhoto = function() {
+            var cameraOptions = {
+                targetWidth: 300,
+                targetHeight: 300
+            };
+            navigator.camera.getPicture(function(imageURI) {
 
-    }, function(err) {
+                $scope.imageURI = imageURI;
+                $state.go('tab.camera');
 
-      alert("Oops!  Can't take your photo!  Either you backed out before saving a photo, or you are not on a device.  Camera will not work from the emulator...");
-    }, cameraOptions);
-  }
-})
+            }, function(err) {
+
+                alert("Oops!  Can't take your photo!  Either you backed out before saving a photo, or you are not on a device.  Camera will not work from the emulator...");
+            }, cameraOptions);
+        }
+    })
+
+
 
     .controller('SignInCtrl', function($scope, $state, $http, $localStorage) {
         $scope.data = {};
@@ -140,60 +139,162 @@ var cameraOptions = {
 
 
 
-      $scope.signIn = function(user) {
+        $scope.signIn = function(user) {
 
-        $http.post("http://api.maggiestcloud.com/login", {email: user.email, password: user.password} )
-          .success(function(data){ console.log(data);
-            if (data.status.length && data.status == "success") {
-                $localStorage.userid = data.userid;
-              console.log("success");
-              $state.go('tab.profile');
-            }
-            else {
-                $scope.data.errorMessage ="could not login";
-              $state.go('signIn');
-            }
-          })
-           .error(function(data){
-            console.log("error");
-             $state.go('signIn', {errorMessage: "Invalid Login"});
-          });
+            $http.post("http://api.maggiestcloud.com/login", {email: user.email, password: user.password} )
+                .success(function(data){ console.log(data);
+                    if (data.status.length && data.status == "success") {
+                        $localStorage.userid = data.userid;
+                        console.log("success");
+                        $state.go('tab.profile');
+                    }
+                    else {
+                        $scope.data.errorMessage ="could not login";
+                        $state.go('signIn');
+                    }
+                })
+                .error(function(data){
+                    console.log("error");
+                    $state.go('signIn', {errorMessage: "Invalid Login"});
+                });
 
-       /* console.log('Sign-In', user);
-        $state.go('tab.dash');*/
-      };
+            /* console.log('Sign-In', user);
+             $state.go('tab.dash');*/
+        };
 
     })
 
-.controller('DashCtrl', function($scope) {})
+    .controller('DashCtrl', function($scope, $cordovaNativeAudio, $cordovaMedia) {
+        $scope.data = {};
+        $scope.data.src = "myrecording.mp3";
 
-.controller('ChatsCtrl', function($scope) {
-})
+        $scope.recordAudio = function() {
+/*
+            navigator.device.capture.captureAudio(function(mediaFiles) {
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
 
-.controller('FriendsCtrl', function($scopeq, Friends) {
-  $scope.friends = Friends.all();
-})
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+                var i, len;
+                for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+                    $scope.data.src = mediaFiles[i].fullPath;
+                    /*var media = $cordovaMedia.newMedia(mediaFiles[i].fullPath).then(function() {
+                     // success
+                     alert("success!");
+                     $scope.data.src = mediaFiles[i].fullPath;
+                     }, function () {
+                     // error
+                     alert("error!");
+                     });*/
 
-/*.controller('ProfileCtrl', function($scope, $stateParams, Profile) {
-        $scope.profile = Profile.get($stateParams.profileId);
-    })
-*/
-    .controller('ProfileCtrl', function($scope, $state) {
-        $scope.gotoaccount = function(){
-            $state.go("tab.account")
+                    /*alert(JSON.stringify(mediaFiles[i]));
+                     alert(mediaFiles[i].fullPath);
+                     $cordovaNativeAudio
+                     .preloadSimple('message', mediaFiles[i].fullPath)
+                     .then(function (msg) {
+                     alert(msg);
+                     }, function (error) {
+                     alert(error);
+                     });*/
+
+                    //uploadFile(mediaFiles[i]);
+                    //alert(JSON.stringify(mediaFiles[i]));
+            /*
+                }
+            }, function(error) {
+                var msg = 'An error occurred during capture: ' + error.code;
+                navigator.notification.alert(msg, null, 'Uh oh!');
+            });*/
+            alert("test");
+        }
+
+        $scope.playAudio = function() {
+
+            // Please note: this code is not working yet
+            alert($scope.data.src);
+            var media = $cordovaMedia.newMedia(cordova.file.applicationDirectory + $scope.data.src).then(function() {
+                // success
+                alert("success!");
+            }, function () {
+                // error
+                alert("error!");
+            });
+            media.play();
+
+            //$cordovaNativeAudio.play('message');
         }
     })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    .controller('ChatsCtrl', function($scope) {
+    })
+
+    .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+    })
+
+    .controller('FriendsCtrl', function($scopeq, Friends) {
+        $scope.friends = Friends.all();
+    })
+
+    .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+        $scope.friend = Friends.get($stateParams.friendId);
+    })
+
+    /*.controller('ProfileCtrl', function($scope, $stateParams, Profile) {
+     $scope.profile = Profile.get($stateParams.profileId);
+     })
+     */
+    .controller('ProfileCtrl', function($scope, $state, $localStorage, $cordovaNativeAudio, $cordovaMedia) {
+        $scope.logout = function(){
+            delete $localStorage.userid;
+            $state.go("signIn");
+
+        }
+        $scope.gotoaccount = function(){
+            $state.go("tab.account")
+        }
+
+        $scope.recordAudio = function() {
+
+             navigator.device.capture.captureAudio(function(mediaFiles) {
+
+
+
+             var i, len;
+             for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+             $scope.data.src = mediaFiles[i].fullPath;
+                 alert(mediaFiles[i].fullPath);
+             /*var media = $cordovaMedia.newMedia(mediaFiles[i].fullPath).then(function() {
+             // success
+             alert("success!");
+             $scope.data.src = mediaFiles[i].fullPath;
+             }, function () {
+             // error
+             alert("error!");
+             });*/
+
+            /*alert(JSON.stringify(mediaFiles[i]));
+             alert(mediaFiles[i].fullPath);
+             $cordovaNativeAudio
+             .preloadSimple('message', mediaFiles[i].fullPath)
+             .then(function (msg) {
+             alert(msg);
+             }, function (error) {
+             alert(error);
+             });*/
+
+            //uploadFile(mediaFiles[i]);
+            //alert(JSON.stringify(mediaFiles[i]));
+
+             }
+             }, function(error) {
+             var msg = 'An error occurred during capture: ' + error.code;
+             navigator.notification.alert(msg, null, 'Uh oh!');
+             });
+        }
+    })
+
+    .controller('AccountCtrl', function($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
+    });
